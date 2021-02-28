edc4cf1815f627f4c6d991ca0918c69ecf516e2c
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-02-28 11:45:08 +0300

    Added endpoints.

--- app/api/server.py
+++ app/api/server.py
@@ -2,24 +2,47 @@ import threading
 import requests
 import argparse
 
+from werkzeug.exceptions import abort
+
 from app.api.utils import config_parser
-from flask import Flask, request
+from flask import Flask, request, jsonify
+
+from app.db.exceptions import UserNotFoundException
+from app.db.interaction.interaction import DbInteraction
 
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, db_host, db_port, user, password, db_name, rebuild_db=False):
         self.host = host
         self.port = port
+
+        self.db_interaction = DbInteraction(
+            host=db_host,
+            port=db_port,
+            user=user,
+            password=password,
+            db_name=db_name,
+            rebuild_db=rebuild_db
+        )
 
         self.app = Flask(__name__)
 
         self.app.add_url_rule('/shutdown', view_func=self.shutdown)
         self.app.add_url_rule('/', view_func=self.get_home)
         self.app.add_url_rule('/home', view_func=self.get_home)
+        self.app.add_url_rule('/add_user_info', view_func=self.add_user_info(), methods=['POST'])
+        self.app.add_url_rule('/get_user_info/<username>', view_func=self.get_user_info)
+        self.app.add_url_rule('/get_user_info/<username>', view_func=self.edit_user_info, methods=['PUT'])
+
+
+        self.app.register_error_handler(404, self.page_not_found())
+
+    def page_not_found(self, error_description):
+        return jsonify(error=str(error_description)), 404
 
     def run_server(self):
         self.server = threading.Thread(target=self.app.run, kwargs={'host': self.host, 'port': self.port})
-        self.server.stt()
+        self.server.start()
         return self.server
 
     def shutdown_server(self):
@@ -32,6 +55,41 @@ class Server:
 
     def get_home(self):
         return 'Hello, api server.'
+
+    def add_user_info(self):
+        request_body = dict(request.json)
+        username = request_body.get('username', None)
+        password = request_body.get('password', None)
+        email = request_body.get('email', None)
+        self.db_interaction.add_user_info(
+            username=username,
+            password=password,
+            email=email
+        )
+        return f'Successfully added {username}', 201
+
+    def get_user_info(self, username):
+        try:
+            user_info = self.db_interaction.get_user_info(username)
+            return user_info, 200
+        except UserNotFoundException:
+            abort(404, description='User not found')
+
+    def edit_user_info(self, username):
+        request_body = dict(request.json)
+        new_username = request_body.get('username', None)
+        new_password = request_body.get('password', None)
+        new_email = request_body.get('email', None)
+        try:
+            self.db_interaction.edit_user_info(
+                username=username,
+                new_username=new_username,
+                new_password=new_password,
+                new_email=new_email
+            )
+            return f'Successfully edited {username}', 200
+        except UserNotFoundException:
+            abort(404, description='User not found')
 
 
 if __name__ == '__main__':
